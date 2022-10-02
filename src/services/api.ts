1b81e6ddd505f22416e5f40e7ee3e80204f21adb
samubalazs <adoptimal@gmail.com>
2022-10-02 15:06:15 +0200

    GRS-005 - Create basic API call

---
+++ src/services/api.ts
@@ -0,0 +1,36 @@
+import axios from "axios"
+import { RepositoryData } from "../types"
+
+async function fetchRepositories() {
+  const { data } = await axios.get(
+    "https://api.github.com/search/repositories?q=topic:typescript&state:open&sort=created&order=asc"
+  )
+  const result =
+    data &&
+    data.items.reduce(
+      (acc: RepositoryData[], curr: any) => [
+        ...acc,
+        {
+          name: curr.name,
+          fullName: curr.full_name,
+          url: curr.html_url,
+          stars: curr.stargazers_count,
+          watchers: curr.watchers,
+          forks: curr.forks,
+          issues: curr.open_issues,
+          description: curr.description,
+          language: curr.language,
+          topics: curr.topics,
+          created: curr.created_at,
+          updated: curr.updated_at,
+          ownerName: curr.owner.login,
+          ownerUrl: curr.owner.url,
+          ownerAvatar: curr.owner.avatar_url,
+        },
+      ],
+      []
+    )
+  return result ?? []
+}
+
+export default fetchRepositories
---
+++ src/types.ts
@@ -0,0 +1,17 @@
+export type RepositoryData = {
+  name: string
+  fullName?: string
+  url: string
+  stars: number
+  watchers: number
+  forks: number
+  issues: number
+  description?: string
+  language?: string
+  topics?: string[]
+  created: string
+  updated: string
+  ownerName: string
+  ownerUrl: string
+  ownerAvatar: string
+}
