8b2bfceee6e532b99fc25404d3197332d9da44c1
samubalazs <adoptimal@gmail.com>
2022-10-02 16:11:01 +0200

    GRS-007 - Add color pool

---
+++ src/constants/colors.ts
@@ -0,0 +1,18 @@
+export const colors = {
+  body: {
+    background: "#D3EBCD",
+    text: "#ffffff",
+  },
+  page: {
+    background: "#AEDBCE",
+    border: "#635666",
+  },
+  normal: {
+    background: "#839AA8",
+    border: "#635666",
+  },
+  active: {
+    background: "#635666",
+    border: "#AEDBCE",
+  },
+}
